70e77711ead55006433fae9a1b0cde126bc5b996
Jeremy Louie <jeremysf74@yahoo.com>
2021-08-25 17:45:45 -0700

    Recent changes

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,12 +1,14 @@
 
 var techSkillsSelected = false;
 function slideIn() {
-    const programmingContent = document.getElementById("programming1");
-    width = 0;
+    const programmingContent = document.getElementById("tech");
+    var width = 0;
+    var opacity = 0;
     var performAction = setInterval(function() {
-        if (width < 30) {
+        if (width < 10) {
             width += 1;
-            programmingContent.style.opacity = "1";
+            opacity += 0.1;
+            programmingContent.style.opacity = opacity;
             programmingContent.style.left = width + "%";
         }
     }, 10);
