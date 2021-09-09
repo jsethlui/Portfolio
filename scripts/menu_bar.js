f1b9cec75db4116b1667576aa9f17d18816e1f33
Jeremy Louie <jeremysf74@yahoo.com>
2021-09-08 20:43:05 -0700

    Small animation changes to menu bar

--- scripts/menu_bar.js
+++ scripts/menu_bar.js
@@ -1,6 +1,16 @@
 
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+});
+
 function toggleMenuBar() {
-    var mobilePageContainer = document.getElementById("hamburger_menu")
+    // Look for .hamburger
+    var hamburger = document.querySelector(".hamburger");
+    var mobilePageContainer = document.getElementById("menu")
+
     if (mobilePageContainer.style.display === "block") {
         mobilePageContainer.style.display = "none"
     } else {    // if menu bar selected
